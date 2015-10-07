2a37bf6af0efa4c4f74f99717128e4a7da14c52f
Pradipta Kr. Banerjee <pradipta.banerjee@gmail.com>
2015-10-07 16:27:24 +0530

    Add provision to run single test case as part of container run

--- container_benchmark_run.sh
+++ container_benchmark_run.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 #This runs benchmark inside the container
-#benchmark_run.sh [ARGS]
+#benchmark_run.sh 'all' or specific test case
 
-ARGS="$@"
-TESTCASES=`ls -d container/*/*run.sh`
+test_dir=${1}
+TESTS=${test_dir:-all}
+
+if [ "${TESTS}" == "all" ]
+then
+    TESTCASES=`ls -d container/*/*run.sh`
+else
+    TESTCASES=`ls -d container/${TESTS}/*run.sh`
+fi
+
 for testcase in ${TESTCASES}
 do
     TEST_NAME=$( dirname ${testcase} | sed 's/container\///g')
@@ -12,7 +20,7 @@ do
     echo "START Testcase: ${TEST_NAME}"
     echo "********************************************************************"
     echo "********************************************************************"
-    ./${testcase} ${ARGS}
+    ./${testcase}
     echo "********************************************************************"
     echo "********************************************************************"
     echo "END Testcase: ${TEST_NAME}"
--- container_run.sh
+++ container_run.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
-#benchmark_run.sh <host_ip>
+#container_benchmark_run.sh <test|all> <host_ip>
 #Host IP is the netserver IP which the container will connect to
 
 
 #Best way is to run it with -net=host and provide a public IP to the container
 #However we are currently running using the default docker bridge
 
-IP=${1}
+TEST=${1}
+IP=${2}
 LOCAL_HOST_IP=`ip addr list eth0 |grep "inet " |cut -d' ' -f6|cut -d/ -f1`
 
 HOST_IP=${LOCAL_HOST_IP:-$IP}
 echo "Host IP for container to connect to ${HOST_IP}"
 #wordpress port=80
 
-CID=`docker run -e NETSERVER_HOST=${HOST_IP} -itd -p 80 --hostname=benchmark.node benchmark/execution`
+CID=`docker run -e NETSERVER_HOST=${HOST_IP} -itd -p 80 --hostname=benchmark.node benchmark/execution ${TEST}`
 CIP=`docker inspect -f '{{ .NetworkSettings.IPAddress }}' ${CID}`
 echo "Container ID: ${CID} Container IP: ${CIP}"
 echo "Container json logs are in /var/lib/docker/containers/${CID}/${CID}-json.log"
