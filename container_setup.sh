c2fc7f90da49d4de606c862072bfa9657414965c
Pradipta Kr. Banerjee <pradipta.banerjee@gmail.com>
2016-04-27 10:23:32 +0530

    Use Ubuntu:trusty docker image from DockerHub for both Intel and Power as the base image

--- container_setup.sh
+++ container_setup.sh
@@ -1,41 +1,4 @@
 #!/bin/bash
 #Prepare container for running the benchmarks
 
-#Create base image for container
-ARCH=`uname -m`
-RELEASE='trusty'
-apt-get install -y debootstrap
-rm -fr /${RELEASE}
-debootstrap --components=main,universe ${RELEASE} /${RELEASE}
-
-#Add update repo
-if [ "${ARCH}" == "x86_64" ]
-then
-    echo " deb http://archive.ubuntu.com/ubuntu ${RELEASE}-updates main" >> /${RELEASE}/etc/apt/sources.list.d/${RELEASE}-updates.list
-else
-    echo "deb http://ports.ubuntu.com/ubuntu-ports ${RELEASE}-updates main" >> /${RELEASE}/etc/apt/sources.list/${RELEASE}-updates.list
-fi
-
-
-#Update the image with latest fixes
-
-#Do the mounting for entering chroot
-mount -o bind /dev /${RELEASE}/dev/
-mount -t sysfs none /${RELEASE}/sys/
-mount -t proc none /${RELEASE}/proc/
-mount -t devpts none /${RELEASE}/dev/pts/
-#Change to chroot environment
-chroot /${RELEASE} apt-get update && apt-get upgrade -y  && apt-get autoremove -y
-
-#Unmount the files after exiting the chroot
-#exit
-umount /${RELEASE}/dev/pts/
-umount /${RELEASE}/dev/
-umount /${RELEASE}/sys/
-umount /${RELEASE}/proc/
-
-rm -f /${RELEASE}/etc/apt/sources.list.d/${RELEASE}-updates.list
-
-tar -C /${RELEASE} -c . | sudo docker import - benchmark/ubuntu:latest
-
 docker build -t benchmark/execution --rm=true .
